12de1e8de79cdae4f8c617a5a672adda899f934a
Bam <wubetty0727@gmail.com>
2019-11-03 11:42:46 +0800

    當年的期中ㄚㄚㄚ

--- myproject2/src/com/www/E3number.java
+++ myproject2/src/com/www/E3number.java
@@ -5,22 +5,28 @@ import java.util.Scanner;
 
 public class E3number {
 	public static void main(String[] args) {
-		/*Random random = new Random();
+		Random random = new Random();
 		int secret = random.nextInt((10)+1);
+		int count = 0;
 		System.out.println(secret);
 		Scanner scanner = new Scanner(System.in);
 		while(true){
 			System.out.println("your guess:");
 			int n = scanner.nextInt();
+			count++;
 			if(n < secret){
 				System.out.println("higher");
 			}else if(n > secret){
 				System.out.println("lower");
 			}else{
-				System.out.printf("Great! The secret number is %d",secret);
+				if(count <= 2){
+					System.out.printf("excellent! The secret number is %d",secret);
+				}else {
+					System.out.printf("Great! The secret number is %d",secret);
+				}
 			    break;
 			}
-		}*/
+		}
 	}
 	
 
